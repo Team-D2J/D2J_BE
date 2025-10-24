2b082a5f0a9f0631e4e450cf0f18fe4cb926c9d3
yellowstarr0323 <ihansaem60@gmail.com>
2025-10-24 13:28:50 +0900

    init

---
+++ src/main/java/com/example/d2j/D2JApplication.java
@@ -0,0 +1,13 @@
+package com.example.d2j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2JApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(D2JApplication.class, args);
+  }
+
+}
